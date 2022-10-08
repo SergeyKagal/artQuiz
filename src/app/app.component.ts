36c940d30be3190c6dc359cd3a36684fb59294a8
SergeyKagal <3461995@gmail.com>
2022-10-08 18:16:03 +0300

    feat: wallpaper change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterEvent, Event } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'artQuiz';
+  isMainScreen = true;
+  constructor(public router: Router) {
+    router.events
+      .pipe(filter((e: Event): e is RouterEvent => e instanceof RouterEvent))
+      .subscribe((e: RouterEvent) => {
+        // Do something
+        this.isMainScreen = e.url === '/' ? true : false;
+      });
+  }
+
+  ngOnInit() {}
 }
