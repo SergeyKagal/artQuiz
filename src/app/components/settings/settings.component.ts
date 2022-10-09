8150d6ca21f9f4568e1da617cf6ae797c20e19bc
SergeyKagal <3461995@gmail.com>
2022-10-09 17:53:16 +0300

    feat: settings done

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,15 +1,31 @@
+import { JsonPipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { ISettings } from 'src/models/settings.model';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  public settings: ISettings;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (localStorage.getItem('settings')) {
+      this.settings = JSON.parse(localStorage.getItem('settings') as string);
+    } else {
+      this.settings = { soundOn: true, timerOn: true, time: 30, volume: 50 };
+    }
   }
 
+  onSubmit = (e: SubmitEvent) => {
+    e.preventDefault();
+    localStorage.setItem('settings', JSON.stringify(this.settings));
+  };
+  onDefault = (e: Event) => {
+    e.preventDefault();
+    this.settings = { soundOn: true, timerOn: true, time: 30, volume: 50 };
+  };
 }
---
+++ src/models/settings.model.ts
@@ -0,0 +1,6 @@
+export interface ISettings {
+  soundOn: boolean;
+  timerOn: boolean;
+  time: number;
+  volume: number;
+}
